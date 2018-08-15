36d43631d4903b0f08652c161ad963fabb186f85
Carlos Alberto Chavez Reyes <carlosalbertochavezreyes@MacBook-Pro-de-Carlos.local>
2018-08-14 23:41:02 -0500

    Leccion 3

---
+++ eShop/Views/Celda_Producto.swift
@@ -0,0 +1,16 @@
+//
+//  Celda_Producto.swift
+//  eShop
+//
+//  Created by Carlos Alberto Chavez Reyes on 14/08/18.
+//  Copyright © 2018 CarlosChavez. All rights reserved.
+//
+
+import UIKit
+
+class Celda_Producto: UICollectionViewCell {
+    @IBOutlet weak var imagen : UIImageView!
+    @IBOutlet weak var titulo : UILabel!
+    @IBOutlet weak var precio : UILabel!
+}
+ 
